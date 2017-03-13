7a8ecba032a0c42a064fb6c284694354e999774a
Charles Peden and Jennifer Beem <jennifer@>
2017-03-13 15:37:18 -0700

    setup initial file structure

---
+++ js/game.js
@@ -0,0 +1,9 @@
+function Game(player_name, score = null) {
+  this.player_name = player_name;
+  this.score = score
+}
+
+Game.prototype.flip = function(first_card, second_card) {
+
+}
+exports.gameModule = Game;
